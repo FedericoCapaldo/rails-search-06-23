1f35ed6c2d6cb9e28d98d13f29b6556b2f6344fe
Federico <federico.capaldo@hotmail.com>
2023-05-31 18:48:33 +0100

    pg search: added multisearch on both movies and tv shows model, using multisearch in movies controller, adjusted view to handle multisearch results

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,13 @@ class Movie < ApplicationRecord
   belongs_to :director
 
   include PgSearch::Model
-  pg_search_scope :search_by_title_and_synopsis,
-    against: [ :title, :synopsis ],
-    associated_against: {
-      director: [:first_name, :last_name]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
+  # pg_search_scope :search_by_title_and_synopsis,
+  #   against: [ :title, :synopsis ],
+  #   associated_against: {
+  #     director: [:first_name, :last_name]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
+  multisearchable against: [:title, :synopsis]
 end
