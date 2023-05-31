afcb095c6ad9bf319cf733be8a9419026d97244a
Federico <federico.capaldo@hotmail.com>
2023-05-31 18:28:53 +0100

    active record: full text search with @@

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,10 @@ class MoviesController < ApplicationController
   def index
     if params[:query].present?
       sql_query = <<~SQL
-        movies.title ILIKE :query
-        OR movies.synopsis ILIKE :query
-        OR directors.first_name ILIKE :query
-        OR directors.last_name ILIKE :query
+        movies.title @@ :query
+        OR movies.synopsis @@ :query
+        OR directors.first_name @@ :query
+        OR directors.last_name @@ :query
       SQL
       @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
